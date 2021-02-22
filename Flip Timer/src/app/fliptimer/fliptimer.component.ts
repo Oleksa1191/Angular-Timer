0e8b162d3ba14d86242cf969521ba5a667d1a9e4
Sil Moelker <smoelker@gmail.com>
2021-02-22 10:59:48 +0100

    FlipTimer refactored to local component instead of global event handler.

--- Flip Timer/src/app/fliptimer/fliptimer.component.ts
+++ Flip Timer/src/app/fliptimer/fliptimer.component.ts
@@ -1,42 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
-declare var FlipDown;
-declare var $:any
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
-  selector: 'app-fliptimer',
-  templateUrl: './fliptimer.component.html',
-  styleUrls: ['./fliptimer.component.scss']
+    selector: 'app-fliptimer',
+    templateUrl: './fliptimer.component.html',
+    styleUrls: ['./fliptimer.component.scss']
 })
-export class FliptimerComponent implements OnInit {
- 
-  ngOnInit() {
-    this.mainJs()
-    
-  }
-  
-  public mainJs() {
-    document.addEventListener('DOMContentLoaded', () => {
+export class FliptimerComponent implements AfterViewInit {
 
-      // Unix timestamp (in seconds) to count down to
-      var twoDaysFromNow = (new Date().getTime() / 1000) + (30 * 2) + 1;
-    
-      // Set up FlipDown
-      var flipdown = new FlipDown(twoDaysFromNow)
-    
-        // Start the countdown
-        .start()
-    
-        // Do something when the countdown ends
-        .ifEnded(() => {
-          console.log('The countdown has ended!');
-        });
-    
-    
-      // Show version number
-      var ver = document.getElementById('ver');
-      ver.innerHTML = flipdown.version;
-    });
-    
-  }
-  
+    public readonly flipDownId = Math.random().toString(36).substring(2);
+
+    private flipDown: any;
+
+    ngAfterViewInit(): void {
+        this.initFlipDown();
+        this.start();
+    }
+
+    public start(): void {
+        this.flipDown.start();
+    }
+
+    private initFlipDown(): void {
+
+        const twoDaysFromNow = (new Date().getTime() / 1000) + (30 * 2) + 1;
+
+        this.flipDown = new FlipDown(twoDaysFromNow, this.flipDownId, {})
+            .ifEnded(() => {
+                console.log('The countdown has ended!');
+            });
+    }
 }
